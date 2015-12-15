e5364482bfe2d4380522c8b78570aa848821aed0
TeisD <teis.degreve@gmail.com>
2015-12-15 14:20:12 +0100

    Fixed path of attachment for new topic

--- local/app/Http/Controllers/BmoocController.php
+++ local/app/Http/Controllers/BmoocController.php
@@ -575,8 +575,8 @@ class BmoocController extends Controller {
             if (Input::file('topic_attachment') && Input::file('topic_attachment')->isValid()) {
                 $extension = strtolower(Input::file('topic_attachment')->getClientOriginalExtension());
                 if (in_array($extension, ['jpg', 'png', 'gif', 'jpeg', 'pdf'])) {
-                    $destinationPath = 'uploads';
-                    $filename = base64_encode(Input::file('topic_attachment')->getClientOriginalName());
+                    $destinationPath = 'uploads/attachments';
+                    $filename = base64_encode(Input::file('topic_attachment')->getClientOriginalName()).'.'.$extension;
                     Input::file('topic_attachment')->move($destinationPath, $filename);
                     //$topic->url = $filename;
                     $topic->attachment = $filename;
