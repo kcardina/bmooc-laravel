333798bdc2fe689968cc62664964ccc8f83124e4
TeisD <teis.degreve@gmail.com>
2016-02-09 14:44:38 +0100

    Thumbnail class

--- local/resources/views/canvas.blade.php
+++ local/resources/views/canvas.blade.php
@@ -17,126 +17,174 @@
             $('#upload').submit(function(e){
                 e.preventDefault();
 
-                if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-                  alert('The File APIs are not fully supported in this browser.');
-                  return;
-                }
-
                 input = document.getElementById('fileinput');
 
-                if (!input) {
-                  alert("Um, couldn't find the fileinput element.");
-                }
-                else if (!input.files) {
-                  alert("This browser doesn't seem to support the `files` property of file inputs.");
-                }
-                else if (!input.files[0]) {
-                  alert("Please select a file before clicking 'Load'");
-                }
-                else {
-                    file = input.files[0];
+                var t_100 = new Thumbnail(input, 100);
+                var t_1000 = new Thumbnail(input, 1000);
 
-                    fr = new FileReader();
-                    fr.onload = function(e){
-
-                        if(file.type.match('image.*')){
-                            var img = new Image();
-                            img.onload = function(){
-                                getThumbnail(img, 100);
-                                getThumbnail(img, 1000);
-                            }
-                            img.src = event.target.result;
-                        }
-
-                        if(file.type.match('application/pdf') || file.type.match('application/x-pdf')){
-                            var pdfAsArray = convertDataURIToBinary(event.target.result);
-                            getPdfThumbnail(pdfAsArray, 100);
-                            getPdfThumbnail(pdfAsArray, 1000);
-                        }
-                    }
-
-                  fr.readAsDataURL(file);
-                }
+                $.when(t_100.generate(), t_1000.generate()).done(function(){
+                    console.log('Succes! Submitting form.');
+                    if(t_100.hasData) $('body').append('<img src="'+t_100.get()+'" />');
+                    if(t_1000.hasData) $('body').append('<img src="'+t_1000.get()+'" />');
+                }).fail(function(data) {
+                    console.log( 'One or more requests failed: ' + data);
+                });
             });
         });
 
-        function getThumbnail(img, s) {
-            var c = document.createElement("canvas");
-            var ctx = c.getContext("2d");
+        var Thumbnail = (function(){
 
-            var ratio = img.height/img.width;
-            if(ratio > 1){ // portrait
-                if(img.height < s) return;
-                c.height = s;
-                c.width = s/ratio;
-            } else{ // landscape
-                if(img.width < s) return;
-                c.width = s;
-                c.height = s*ratio;
+            var BASE64_MARKER = ';base64,';
+
+            // Constructor
+            function Thumbnail(el, size){
+                this.dfd = new $.Deferred();
+                this.c = document.createElement("canvas");
+                this.ctx = this.c.getContext("2d");
+                this.el = el;
+                this.size = size;
+                this.hasData = false;
             }
 
-            ctx.drawImage(img, 0, 0, c.width, c.height);
+            Thumbnail.prototype.generate = function(){
+                var support = browserSupport.call(this);
+                if(support.isSupported){
+                    readFile.call(this);
+                } else {
+                    this.dfd.reject(support.msg);
+                }
+                return this.dfd.promise();
+            }
 
-            store(c);
-        }
 
-        function getPdfThumbnail(url, s){
+            Thumbnail.prototype.get = function(){
+                return this.c.toDataURL('image/png');
+            }
 
-            PDFJS.workerSrc = '/js/pdf.worker.js';
-            var c = document.createElement("canvas");
-            var ctx = c.getContext("2d");
-
-            PDFJS.getDocument(url).then(function getPdfHelloWorld(pdf) {
-                pdf.getPage(1).then(function getPageHelloWorld(page) {
-                    var viewport = page.getViewport(1);
-
-                    var ratio = viewport.height/viewport.width;
-                    if(ratio > 1){ // portrait -> s = max height
-                        var scale = s / viewport.height;
-                        viewport = page.getViewport(scale);
-                    } else{ // landscape -> s = max width
-                        var scale = s / viewport.width;
-                        viewport = page.getViewport(scale);
+            function readFile(){
+                var file = this.el.files[0];
+                var fr = new FileReader();
+                var pointer = this;
+                fr.onload = function(e){
+                    if(file.type.match('image.*')){
+                        var img = new Image();
+                        img.onload = function(){
+                            render.call(pointer, img);
+                        }
+                        img.src = event.target.result;
+                    } else if(file.type.match('application/pdf') || file.type.match('application/x-pdf')){
+                        var pdfAsArray = convertDataURIToBinary(event.target.result);
+                        renderPDF.call(pointer, pdfAsArray);
+                    } else {
+                        this.dfd.reject("The uploaded file was not an image, nor a pdf");
                     }
+                }
 
-                    c.height = viewport.height;
-                    c.width = viewport.width;
+                fr.readAsDataURL(file);
+            }
 
-                    //
-                    // Render PDF page into canvas context
-                    //
-                    var renderContext = {
-                        canvasContext: ctx,
-                        viewport: viewport
-                    };
+            function render(img){
+                var ratio = img.height/img.width;
+                if(ratio > 1){ // portrait
+                    if(img.height < this.size){ this.dfd.resolve(); return;}
+                    this.c.height = this.size;
+                    this.c.width = this.size/ratio;
+                } else{ // landscape
+                    if(img.width < this.size){ this.dfd.resolve(); return;}
+                    this.c.width = this.size;
+                    this.c.height = this.size*ratio;
+                }
 
-                    var renderTask = page.render(renderContext);
+                this.ctx.drawImage(img, 0, 0, this.c.width, this.c.height);
+                this.hasData = true;
+                this.dfd.resolve();
+            }
 
-                    renderTask.promise.then(function(){
-                        store(c);
+            function renderPDF(url){
+                var size = this.size;
+                var ctx = this.ctx;
+                PDFJS.workerSrc = '/js/pdf.worker.js';
+
+                PDFJS.getDocument(url).then(function getPdfHelloWorld(pdf) {
+                    pdf.getPage(1).then(function getPageHelloWorld(page) {
+                        var viewport = page.getViewport(1);
+                        var ratio = viewport.height/viewport.width;
+                        if(ratio > 1){ // portrait -> s = max height
+                            var scale = size / viewport.height;
+                            viewport = page.getViewport(scale);
+                        } else{ // landscape -> s = max width
+                            var scale = size / viewport.width;
+                            viewport = page.getViewport(scale);
+                        }
+
+                        c.height = viewport.height;
+                        c.width = viewport.width;
+
+                        // Render PDF page into canvas context
+                        var renderContext = {
+                            canvasContext: ctx,
+                            viewport: viewport
+                        };
+
+                        var renderTask = page.render(renderContext);
+
+                        renderTask.promise.then(function(){
+                            this.hasData = true;
+                            this.dfd.resolve();
+                        });
                     });
                 });
-            });
-        }
+            }
 
-        function store(c){
-            $('body').append(c);
-        }
+            function browserSupport(){
+                var support = {
+                    isSupported: true,
+                    msg: 'Browser supports thumbnail generation.'
+                }
 
-        var BASE64_MARKER = ';base64,';
+                if (!window.File || !window.FileReader || !window.FileList || !window.Blob){
+                        support = {
+                            isSuported: false,
+                            msg: 'The File APIs are not fully supported in this browser.'
+                        }
+                    }
+                    if (!this.el) {
+                        support = {
+                            isSuported: false,
+                            msg: 'Couldn\'t find the fileinput element.'
+                        }
+                    }
+                    if (!this.el.files) {
+                        support = {
+                            isSuported: false,
+                            msg: 'This browser doesn\'t seem to support the \'files\' property of file inputs.'
+                        }
+                    }
+                    if (!this.el.files[0]) {
+                        support = {
+                            isSuported: false,
+                            msg: 'No file selected.'
+                        }
+                    }
+                return support;
+            }
 
-        function convertDataURIToBinary(dataURI) {
-          var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-          var base64 = dataURI.substring(base64Index);
-          var raw = window.atob(base64);
-          var rawLength = raw.length;
-          var array = new Uint8Array(new ArrayBuffer(rawLength));
+            function convertDataURIToBinary(dataURI) {
+                var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+                var base64 = dataURI.substring(base64Index);
+                var raw = window.atob(base64);
+                var rawLength = raw.length;
+                var array = new Uint8Array(new ArrayBuffer(rawLength));
 
-          for(var i = 0; i < rawLength; i++) {
-            array[i] = raw.charCodeAt(i);
-          }
-          return array;
-        }
+                for(var i = 0; i < rawLength; i++) {
+                    array[i] = raw.charCodeAt(i);
+                }
+                return array;
+            }
+
+            return Thumbnail;
+
+        })();
 
     </script>
 
