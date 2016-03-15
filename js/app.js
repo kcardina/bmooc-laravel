4896183bf41746e24a77f8f5efb74924edc50d7d
TeisD <teis.degreve@gmail.com>
2016-03-15 16:38:02 +0100

    Fix zoom behaviour on IE

--- js/app.js
+++ js/app.js
@@ -655,6 +655,7 @@ var Tree = (function(){
         d3.select(window).on("mouseup.zoom", function(){
             d3.select(window).on("mousemove.zoom", null).on("mouseup.zoom", null);
         });
+        return true;
     }
 
     /**
